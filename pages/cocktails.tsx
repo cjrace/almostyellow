b5de323fe6a89915cbed86e7f77b725f6ab7ab3f
cjrace <52536248+cjrace@users.noreply.github.com>
2024-09-08 22:06:31 +0100

    Move to buttons for navigation

--- pages/cocktails.tsx
+++ pages/cocktails.tsx
@@ -46,9 +46,7 @@ const CocktailsPage: React.FC = () => {
         </ul>
 
         <div className={styles.ctas}>
-          <Link href="/" className={styles.secondary}>
-            Back to homepage
-          </Link>
+          <Link href="/" className={styles.secondary}>Back to homepage</Link>
         </div>
 
       </main>
