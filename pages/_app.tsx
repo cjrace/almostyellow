8ac9b0be44323584ebe39cea76c89333eb85093a
lauraselby <laura.selby@hotmail.co.uk>
2024-09-09 00:14:34 +0100

    adding some style and dark mode option

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,19 @@ import '../app/globals.css';
 import Head from 'next/head';
 import { AppProps } from 'next/app';
 
+const toggleDarkMode = () => {
+  document.body.classList.toggle('dark-mode');
+};
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>Almost yellow</title>
       </Head>
+
+      <button onClick={toggleDarkMode}>Toggle Dark Mode</button>
+      
       <Component {...pageProps} />
     </>
   )
