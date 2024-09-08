c6321482edd0ce1aede41b9df06861d3bd2f8162
lauraselby <laura.selby@hotmail.co.uk>
2024-09-08 20:16:32 +0100

    Added some cocktail recipes

--- components/cocktails/cocktails.ts
+++ components/cocktails/cocktails.ts
@@ -1,46 +1,130 @@
 // Create the options for the cocktails page, could be a database table eventually
 export const cocktails = [
     {
-        name: 'Old Fashioned',
-        spirits: ['Bourbon'],
-        rating: 2.0 as number, // Rated out of 5
-        photoUrl: "/images/cocktail-placeholder.jpg",
-        recipe: {
-            ingredients: ['Bourbon', 'Sugar', 'Water', 'Bitters'],
-            instructions:
-                '1. Add a load of crushed ice to the cocktail shaker.\n' +
-                '2. Add bourbon, sugar, water, and bitters.\n' +
-                '3. Shake loads.\n' +
-                '4. Pour into glasses and enjoy.',
-        },
-    },
-    {
-        name: 'Cocktail 2',
-        spirits: ['Rum'],
-        rating: 4.5 as number, // Rated out of 5
-        photoUrl: "/images/cocktail-placeholder.jpg",
-        recipe: {
-            ingredients: ['Rum', 'Something fruity', 'Ice'],
-            instructions:
-                '1. Add a load of crushed ice to the cocktail shaker.\n' +
-                '2. Add alcohol and then something fruity.\n' +
-                '3. Shake loads.\n' +
-                '4. Pour into glasses and enjoy.',
-        },
-    },
-    {
-        name: 'Cocktail 3',
-        spirits: ['Rum', 'Bourbon'],
+        name: 'Stone cold margarita',
+        spirits: ['Tequila'],
         rating: 5.0 as number, // Rated out of 5
         photoUrl: "/images/cocktail-placeholder.jpg",
         recipe: {
-            ingredients: ['Rum', 'Bourbon', 'Something fruity', 'Ice'],
-            instructions:
-                '1. Add a load of crushed ice to the cocktail shaker.\n' +
-                '2. Add alcohol and then something fruity.\n' +
-                '3. Shake loads.\n' +
-                '4. Pour into glasses and enjoy.',
+            ingredients: ['2 x Tequila', '1 x Triple sec', '1 x Lime juice', 'Few drops Agave syrup', '1 x Grand marnier'],
+            instructions: [
+                'Need to redo this to retest the method',
+                'Add Tequila, triple sec, lime juice and agave syrup to a cocktail shaker with ice',
+                'Shake loads',
+                'Add more ice to glass',
+                'Pour over ice and top with grand marnier',
+                ]
         },
     },
+    {
+        name: 'Toffspresso martini',
+        spirits: ['Vodka', 'Kahlua'],
+        rating: 5.0 as number, // Rated out of 5
+        photoUrl: "/images/cocktail-placeholder.jpg",
+        recipe: {
+            ingredients: ['2 x Toffee vodka', '1 x Khalua (or Tia Maria)', '1 x Espresso pod'],
+            instructions: [
+                'Need to redo this to retest the method',
+                'Make espresso and chill in the fridge for a while if you can',
+                'Add espresso, toffee vodka and khalua to a cocktail shaker with ice',
+                'Shake as much as you possibly can',
+                'Pour into glass',
+                ]
+        },
+    },
+    {
+        name: 'Rum sunset',
+        spirits: ['Rum'],
+        rating: 5.0 as number, // Rated out of 5
+        photoUrl: "/images/cocktail-placeholder.jpg",
+        recipe: {
+            ingredients: ['2 x Spiced rum', '4 x Orange juice (or just top to preference)', '1 x Grenadine'],
+            instructions: [
+                'Need to redo this to retest the method',
+                'Add ice to a glass (even better if you use the tiki ones)',
+                'Pour orange juice over rum and then slowly pour grenadine on top'
+                ]
+        },
+    },
+    {
+        name: 'Gin fizz',
+        spirits: ['Gin'],
+        rating: 4.0 as number, // Rated out of 5
+        photoUrl: "/images/cocktail-placeholder.jpg",
+        recipe: {
+            ingredients: ['2 x Gin', '1 x Lemon juice', '1 x Sugar syrup', '1 x Egg white', 'Soda water'],
+            instructions: [
+                'Need to redo this to retest the method',
+                'Pour the gin, lemon juice, egg white and sugar syrup in to a cocktail shaker',
+                'Shake well',
+                'Add ice cubes to the shaker and then shake again as much as you can',
+                'Add ice to a glass',
+                'Strain into glass and top up with a dash of soda water'
+                ]
+        },
+    },
+    {
+        name: 'Baileys milkshake',
+        spirits: ['Baileys'],
+        rating: 5.0 as number, // Rated out of 5
+        photoUrl: "/images/cocktail-placeholder.jpg",
+        recipe: {
+            ingredients: ['2 x Baileys', '3 x Chocomel (or milk)', 'Vanilla ice cream'],
+            instructions: [
+                'Need to redo this to retest the method',
+                'Blend together baileys, ice cream and chocomel until it looks tasty',
+                '[Alternative - add a scoop of ice cream to a glass and pour baileys and chocomel over it]'
+                
+                ]
+        },
+    },
+    {
+        name: 'Mai tai',
+        spirits: ['Rum'],
+        rating: 4.0 as number, // Rated out of 5
+        photoUrl: "/images/cocktail-placeholder.jpg",
+        recipe: {
+            ingredients: ['1 x White rum', '1 x Spiced rum', '1 x Orange curacao', '1 x Orgeat syrup', '1 x Lime juice', '2 x Pineapple juice', 'Grenadine (optional)'],
+            instructions: [
+                'Need to redo this to retest the method',
+                'Add all ingredients to a cocktail shaker with ice',
+                'Shake loads',
+                'Pour into glass',
+                'Optional - pour a dash of grenadine on top once in the glass'
+                ]
+        },
+    },
+    {
+        name: 'Bramble',
+        spirits: ['Gin'],
+        rating: 5.0 as number, // Rated out of 5
+        photoUrl: "/images/cocktail-placeholder.jpg",
+        recipe: {
+            ingredients: ['2 x Gin', '1 x Lemon juice', '1 x Sugar syrup', '1 x Fruity liquer (chambord, creme de chassis etc)'],
+            instructions: [
+                'Need to redo this to retest the method',
+                'Add all ingredients to a cocktail shaker with ice',
+                'Shake loads',
+                'Pour into glass'
+                ]
+        },
+    },
+    {
+        name: 'Tequila sunrise',
+        spirits: ['Tequila'],
+        rating: 4.0 as number, // Rated out of 5
+        photoUrl: "/images/cocktail-placeholder.jpg",
+        recipe: {
+            ingredients: ['2 x Tequila', '1 x Triple sec', '1 x Lemon juice', '4 x orange juice', 'Grenadine'],
+            instructions: [
+                'Need to redo this to retest the method',
+                'Add a bit of grenadine to a glass with ice',
+                'Add all other ingredients to a cocktail shaker with ice',
+                'Shake loads',
+                'Pour slowly into glass on top of grenadine'
+                ]
+        },
+    },
+
     // Add more cocktails...
 ];
