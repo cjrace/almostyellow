e047451c8b0050709b34fb0ace8e0fca51867b07
lauraselby <laura.selby@hotmail.co.uk>
2024-09-12 22:39:35 +0100

    remove one confetti thing

--- pages/cocktails.tsx
+++ pages/cocktails.tsx
@@ -30,9 +30,6 @@ const CocktailsPage: React.FC = () => {
         
         <h1>Cocktails</h1>
         
-        <script src="https://run.confettipage.com/here.js"
-          data-confetticode="U2FsdGVkX193OrwhDdQijJwJq+gY+clAC4OsNYdgWOMqe/yukJzjxwJiqWXUevIbeRzYFVoVr1tRJiuyd71Yy5z7dkuOkaPo/MOgDsBsBVZYOblIczhO/CYfkYJxNKOJuCAFHpvQeFZqd9FSp9ccxdJCfMTfTWUj18p9MBriUdi1r08UftcTuO8onusTIh7Fz2phQpniZqcrVIR58TzxZFWH3g1uBOo1pkcizcVKpkdFHV8chSkfLPZIiWezQml/fOpJ8DoDXUyqSN3kuliUFNxV5FiQhSgAdtgb/kJA/1X+e6ZDJmBklX7pKez+5Y0fbLxwOMDACrg1UE4WOopbAch4HKdSQceKBzcoPFW6LjHJM8pXfNfrv/m9kyK+0jnM3eq1HAV2fmnSu8k5ByDT8BMAY6xRpOl5DJAAUS+cHGWZC1V07BNgSuZf+MxDsSpnETxw/wZwz8Z4Uxz8dXg2eGfar4KUV5pBZr+L9dv635001DSbaqt9c0PTGywWnRia2pzuwHdlUlLsOivC2V1XITd0jkzSV3VQSjkuOdZLemOn8GZDs0yt+E8MqGthCt2hDLzgE2HO1bJWO6i+sUGAuyzIIj0X2aSEC5V6C4mG52bHoLO1h8kA56yMrXFQ8GlLXheVXwQif/GS/t+RTCroV8WaPNMe7E9hdesdX+tC51D2dTSdeoNrewEdZ/sqZYyv"></script>
-        
         {/* Dropdown selector */}
         <form onSubmit={handleSubmit(handleFormSubmit)}>
           <p>Filter by spirit:</p>
