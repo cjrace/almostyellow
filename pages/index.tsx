42f0f7722ec37995bb06bd3fd29e5baa48f35ef1
cjrace <52536248+cjrace@users.noreply.github.com>
2024-09-12 19:01:31 +0100

    initial basic random selector

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,8 @@ export default function Home() {
 
         <div className={styles.ctas}>
           <Link className={styles.primary} href="/games">View our games</Link>
-          <Link className={styles.primary} href="/">View our recipes</Link>
+          <Link className={styles.primary} href="/decisionmaker">Random decision picker</Link>
+          <Link className={styles.primary} href="/recipes">Our recipes</Link>
           <Link className={styles.primary} href="/cocktails">Cocktail time!</Link>
         </div>
       </main>
