6018dfb103e8caa4071dffc709bf2183a2d4163c
lauraselby <laura.selby@hotmail.co.uk>
2024-09-14 21:28:59 +0100

    lots of faffing to get mantine theme to do something

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,26 @@
-import '../app/globals.css';
-import Head from 'next/head';
 import { AppProps } from 'next/app';
-import Script from 'next/script'
-import React from 'react';
+import Head from 'next/head';
+import Script from 'next/script';
+import { MantineProvider, useMantineColorScheme} from '@mantine/core';
+import { theme } from '../theme';
+import { ColorSchemeToggle } from '../components/ColorSchemeToggle';
 import '@mantine/core/styles.css';
-import { MantineProvider } from '@mantine/core';
-
-const toggleDarkMode = () => {
-  document.body.classList.toggle('dark-mode');
-};
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    
+    <MantineProvider theme={theme}>
+
+    <ColorSchemeToggle />
+
       <Head>
         <title>Almost yellow</title>
       </Head>
 
-      <Script src="https://www.clarity.ms/tag/o05b82hwae" type="text/javascript"/>
-
-      <MantineProvider>
-      <button onClick={toggleDarkMode}>Toggle Dark Mode</button>
+      <Script src="https://www.clarity.ms/tag/o05b82hwae" type="text/javascript" />
 
       <Component {...pageProps} />
-      </MantineProvider>
-
-    </>
+    </MantineProvider> 
   );
 }
 
