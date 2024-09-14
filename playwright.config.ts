2f30dae56baf9d13557596b2209f148e21b68f91
cjrace <52536248+cjrace@users.noreply.github.com>
2024-09-14 14:26:57 +0100

    Make tests build by default, reduce browsers tested and update nav test

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,21 +33,6 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-
     /* Test against mobile viewports. */
     {
        name: 'Mobile Chrome',
@@ -59,10 +44,6 @@ export default defineConfig({
     },
 
     /* Test against branded browsers. */
-    {
-      name: 'Microsoft Edge',
-      use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    },
     {
       name: 'Google Chrome',
       use: { ...devices['Desktop Chrome'], channel: 'chrome' },
