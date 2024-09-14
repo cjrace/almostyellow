5117e44ddd7f725aedec887658b47c8efc8a65e2
cjrace <52536248+cjrace@users.noreply.github.com>
2024-09-14 13:10:32 +0100

    Rewrite the existing tests
    Done following reading the opening playwright guidance online

--- tests/initial_load.spec.ts
+++ tests/initial_load.spec.ts
@@ -2,6 +2,8 @@ import { test, expect } from '@playwright/test';
 
 test('Homepage has the title "Almost yellow"', async ({ page }) => {
   await page.goto('/');
+  // Check the h1 exists
   await expect(page.locator('h1')).toContainText('Almost yellow');
+  // Check the overall website title exists
   await expect(page).toHaveTitle(/Almost yellow/);
 });
