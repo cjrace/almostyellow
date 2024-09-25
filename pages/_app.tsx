91af7c7d6de55e60fec2bc3c3cade1242d9c9c5b
cjrace <52536248+cjrace@users.noreply.github.com>
2024-09-25 23:03:00 +0100

    run prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
 import Script from "next/script";
+import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import { theme } from "../theme";
 import { ColorSchemeToggle } from "../components/ColorSchemeToggle";
-import "@mantine/core/styles.css";
+import React from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
