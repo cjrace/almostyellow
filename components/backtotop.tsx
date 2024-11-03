879432f43295c173b3d49db54a7f042dd0938b00
Cam Race <52536248+cjrace@users.noreply.github.com>
2024-11-03 12:20:37 +0000

    change back to top to use affix (#49)

--- components/backtotop.tsx
+++ components/backtotop.tsx
@@ -1,20 +1,28 @@
 import React from "react";
-import { Button } from "@mantine/core";
+import { useWindowScroll } from "@mantine/hooks";
+import { Button, Transition, rem, Affix } from "@mantine/core";
+import { IconArrowUp } from "@tabler/icons-react";
 
-interface BackToTopButtonProps {
-  className?: string;
-}
-
-const BackToTopButton: React.FC<BackToTopButtonProps> = ({ className }) => {
-  const handleBackToTop = () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  };
+const BackToTop: React.FC = () => {
+  const [scroll, scrollTo] = useWindowScroll();
 
   return (
-    <Button onClick={handleBackToTop} className={className}>
-      Back to Top
-    </Button>
+    <Affix position={{ bottom: 20, right: 20 }}>
+      <Transition transition="slide-up" mounted={scroll.y > 0}>
+        {(transitionStyles) => (
+          <Button
+            leftSection={
+              <IconArrowUp style={{ width: rem(16), height: rem(16) }} />
+            }
+            style={transitionStyles}
+            onClick={() => scrollTo({ y: 0 })}
+          >
+            Back to top
+          </Button>
+        )}
+      </Transition>
+    </Affix>
   );
 };
 
-export default BackToTopButton;
+export default BackToTop;
