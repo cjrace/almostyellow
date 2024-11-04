629ff21fff37d93c6a5512ac5c7755d56a351047
cjrace <52536248+cjrace@users.noreply.github.com>
2024-11-04 11:33:06 +0000

    add frederick as favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -31,11 +31,31 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider defaultColorScheme="dark" theme={acalat}>
       <Head>
+        {"General metadata"}
         <title>Almost yellow</title>
         <meta
           name="description"
           content="Our website of games, cocktails and the good stuff."
         />
+
+        {"Favicons"}
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="AlmostYellow" />
+        <link rel="manifest" href="/site.webmanifest" />
+
+        {"Add more here..."}
       </Head>
 
       <Component {...pageProps} />
