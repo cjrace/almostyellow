af66964864e93c0bebdb85fb84e2dc609a17b62e
cjrace <52536248+cjrace@users.noreply.github.com>
2024-11-06 20:09:44 +0000

    remove = from csp

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,16 @@
 import type { NextConfig } from "next";
 
 const cspHeader = `
-  img-src 'self' blob: data:;
-  font-src 'self';
-  object-src 'none';
-  base-uri 'self';
-  form-action 'self';
-  frame-ancestors 'none';
-  upgrade-insecure-requests;
-  referrer-policy 'no-referrer-when-downgrade'; 
-  x-content-type-options 'nosniff';
-  permissions-policy geolocation 'none', microphone 'none', camera 'none';   
+img-src 'self' blob: data:;
+font-src 'self';
+object-src 'none';
+base-uri 'self';
+form-action 'self';
+frame-ancestors 'none';
+upgrade-insecure-requests;
+referrer-policy 'no-referrer-when-downgrade';
+x-content-type-options 'nosniff';
+permissions-policy geolocation='none', microphone='none', camera='none';  
 `;
 
 const nextConfig: NextConfig = {
