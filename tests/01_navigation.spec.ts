db28b3a31ea2928891c38bcf73b45f7114222dda
cjrace <52536248+cjrace@users.noreply.github.com>
2024-11-06 15:49:23 +0000

    add tests for title changes

--- tests/01_navigation.spec.ts
+++ tests/01_navigation.spec.ts
@@ -5,10 +5,12 @@ test("Can navigate to the cocktails page", async ({ page }) => {
 
   await page.getByRole("link", { name: "Cocktail time!" }).click();
   await expect(page).toHaveURL("/cocktails");
+  await expect(page).toHaveTitle("Cocktails | Almost Yellow");
   await expect(page.locator("h1")).toContainText("Cocktails");
 
   await page.getByRole("link", { name: "Home" }).click();
   await expect(page).toHaveURL("/");
+  await expect(page).toHaveTitle("Almost Yellow");
 });
 
 test("Can navigate to the games page and through its subpages", async ({
@@ -18,20 +20,24 @@ test("Can navigate to the games page and through its subpages", async ({
 
   await page.getByRole("link", { name: "View our games" }).click();
   await expect(page).toHaveURL("/games");
+  await expect(page).toHaveTitle("Games | Almost Yellow");
   await expect(page.locator("h1")).toContainText("Our games");
 
   await page.getByRole("link", { name: "Irish Bingo" }).click();
+  await expect(page).toHaveTitle("Irish Bingo | Almost Yellow");
   await expect(page).toHaveURL("/games/irishbingo");
   await expect(page.locator("h1")).toContainText("Irish Bingo");
 
   await page.getByRole("link", { name: "Games" }).click();
   await page.getByRole("link", { name: "Uno" }).click();
+  await expect(page).toHaveTitle("Uno | Almost Yellow");
   await expect(page).toHaveURL("/games/uno");
   await expect(page.locator("h1")).toContainText("Uno");
 
   await page.getByRole("link", { name: "Games" }).click();
   await page.getByRole("link", { name: "Home" }).click();
   await expect(page).toHaveURL("/");
+  await expect(page).toHaveTitle("Almost Yellow");
 });
 
 test("Navigate to the decision maker page", async ({ page }) => {
@@ -39,10 +45,12 @@ test("Navigate to the decision maker page", async ({ page }) => {
 
   await page.getByRole("link", { name: "How we make decisions" }).click();
   await expect(page).toHaveURL("/decisionmaker");
+  await expect(page).toHaveTitle("Decision Maker | Almost Yellow");
   await expect(page.locator("h1")).toContainText("The Decision Maker");
 
   await page.getByRole("link", { name: "Home" }).click();
   await expect(page).toHaveURL("/");
+  await expect(page).toHaveTitle("Almost Yellow");
 });
 
 test("Navigate to the holidays page", async ({ page }) => {
@@ -50,10 +58,12 @@ test("Navigate to the holidays page", async ({ page }) => {
 
   await page.getByRole("link", { name: "Our holidays!" }).click();
   await expect(page).toHaveURL("/holidays");
+  await expect(page).toHaveTitle("Holidays | Almost Yellow");
   await expect(page.locator("h1")).toContainText("Our trips and holidays");
 
   await page.getByRole("link", { name: "Home" }).click();
   await expect(page).toHaveURL("/");
+  await expect(page).toHaveTitle("Almost Yellow");
 });
 
 test("Navigate through admin pages", async ({ page }) => {
@@ -61,17 +71,21 @@ test("Navigate through admin pages", async ({ page }) => {
 
   await page.getByRole("link", { name: "Admin stuff" }).click();
   await expect(page).toHaveURL("/admin");
+  await expect(page).toHaveTitle("Admin | Almost Yellow");
   await expect(page.locator("h1")).toContainText("Welcome to our admin page");
 
   await page.getByRole("link", { name: "Chopin Liszt" }).click();
   await expect(page).toHaveURL("/admin/chopinliszt");
+  await expect(page).toHaveTitle("Chopin Liszt | Almost Yellow");
   await expect(page.locator("h1")).toContainText("Chopin Liszt");
 
   await page.getByRole("link", { name: "Admin" }).click();
   await expect(page).toHaveURL("/admin");
+  await expect(page).toHaveTitle("Admin | Almost Yellow");
 
   await page.getByRole("link", { name: "Home" }).click();
   await expect(page).toHaveURL("/");
+  await expect(page).toHaveTitle("Almost Yellow");
 });
 
 test("Jumping to home from 3 layer breadcrumb", async ({ page }) => {
@@ -79,4 +93,5 @@ test("Jumping to home from 3 layer breadcrumb", async ({ page }) => {
 
   await page.getByRole("link", { name: "Home" }).click();
   await expect(page).toHaveURL("/");
+  await expect(page).toHaveTitle("Almost Yellow");
 });
