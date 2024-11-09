ac2e7a9381edac5024af0bd67e02683cdc3ee5ba
cjrace <52536248+cjrace@users.noreply.github.com>
2024-11-09 14:18:21 +0000

    a bit better, can access all pages while logged in

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -2,18 +2,24 @@ import type { NextAuthConfig } from "next-auth";
 
 export const authConfig = {
   pages: {
-    signIn: "/login",
+    signIn: "/admin/login",
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
+
+      // Redirect users to admin if they are logged in and hit the login page
+      const isInAdminLoginPage = nextUrl.pathname === "/admin/login";
+      if (isLoggedIn && isInAdminLoginPage) {
+        return Response.redirect(new URL("/admin", nextUrl));
+      }
+
       const isInAdmin = nextUrl.pathname.startsWith("/admin");
       if (isInAdmin) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL("/admin", nextUrl));
       }
+
       return true;
     },
   },
