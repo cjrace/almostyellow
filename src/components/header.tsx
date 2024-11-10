948ae42b7d2f8259162e06ffee85f62608c3d5ab
cjrace <52536248+cjrace@users.noreply.github.com>
2024-11-10 14:06:22 +0000

    make colour toggle ignore hydration errors

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,19 @@
-import { Group, Breadcrumbs, Anchor } from "@mantine/core";
-import HeaderIcons from "@/components/headericons";
+"use client"; // needs to be for useMantineColorScheme
+
+import {
+  Group,
+  Breadcrumbs,
+  Anchor,
+  ActionIcon,
+  useMantineColorScheme,
+  useComputedColorScheme,
+  Tooltip,
+} from "@mantine/core";
+import { IconBrandGithub, IconConfetti } from "@tabler/icons-react";
+import playConfetti from "@/components/playconfetti";
+import { IconMoon, IconSun } from "@tabler/icons-react";
+import cx from "clsx";
+import classes from "@/styles/togglecolour.module.css";
 
 interface BreadcrumbItem {
   title: string;
@@ -13,6 +27,10 @@ interface HeaderProps {
 
 export default function Header({ crumbs, noCrumbs = false }: HeaderProps) {
   const mainJustify = noCrumbs ? "flex-end" : "space-between"; // This keeps the icon buttons on the right when there's no crumbs
+  const { setColorScheme } = useMantineColorScheme();
+  const computedColorScheme = useComputedColorScheme("dark", {
+    getInitialValueInEffect: true,
+  });
 
   return (
     <header>
@@ -30,7 +48,57 @@ export default function Header({ crumbs, noCrumbs = false }: HeaderProps) {
         )}
 
         <Group h="100%" gap="xs" justify="flex-end">
-          <HeaderIcons />
+          <Tooltip label="Unleash confetti" openDelay={250}>
+            <ActionIcon
+              onClick={playConfetti}
+              variant="default"
+              size="xl"
+              aria-label="Unleash confetti"
+            >
+              <IconConfetti />
+            </ActionIcon>
+          </Tooltip>
+
+          <Tooltip
+            label={`${computedColorScheme === "dark" ? "Light" : "Dark"} mode`}
+            openDelay={250}
+          >
+            <ActionIcon
+              onClick={() =>
+                setColorScheme(
+                  computedColorScheme === "light" ? "dark" : "light",
+                )
+              }
+              variant="default"
+              size="xl"
+              aria-label="Toggle color scheme"
+            >
+              <IconSun
+                suppressHydrationWarning={true}
+                // Ignoring hydration warning as would rather it rerender server side if the toggle has been changed
+                // Alternative I had was a client side load of the icon but that was noticeably slower / icon would appear on delay
+                className={cx(computedColorScheme == "dark" && classes.hide)}
+              />
+              <IconMoon
+                suppressHydrationWarning={true}
+                className={cx(computedColorScheme == "light" && classes.hide)}
+              />
+            </ActionIcon>
+          </Tooltip>
+
+          <Tooltip label="Source code" openDelay={250}>
+            <ActionIcon
+              component="a"
+              href="https://github.com/cjrace/almostyellow"
+              size="xl"
+              aria-label="Open GitHub repository in a new tab"
+              target="_blank"
+              variant="default"
+              rel="noopener noreferrer"
+            >
+              {<IconBrandGithub />}
+            </ActionIcon>
+          </Tooltip>
         </Group>
       </Group>
     </header>
