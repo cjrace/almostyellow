138109e249db1bfed007c629cec8431c9e519f9c
cjrace <52536248+cjrace@users.noreply.github.com>
2024-11-10 18:51:42 +0000

    remove excess imports from homepage

--- src/app/(homepage)/page.tsx
+++ src/app/(homepage)/page.tsx
@@ -1,4 +1,4 @@
-import { Button, Group, Title, Text, Space, Stack } from "@mantine/core";
+import { Button, Group, Title, Space } from "@mantine/core";
 import {
   IconDeviceGamepad2,
   IconMoodPuzzled,
